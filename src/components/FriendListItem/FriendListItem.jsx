03b6f599acb426c132a19def59dcc2db89ae3da2
astasiempre <ero1494651@gmail.com>
2023-09-17 13:13:04 +0300

    update files

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,23 +1,12 @@
-// export default function FriendList({ friends }) {
-//   return (
-//     <ul className="friend-list">
-//       {friends.map(({ avatar, name, isOnline, id }) => (
-//         <li key={id} className="item">
-//           <span className="status"></span>
-//           <img className="avatar" src={avatar} alt="User avatar" width="48" />
-//           <p className="name">{name}</p>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
+
 
 import css from './FriendlistItem.module.css'
 
-export default function FriendListItem({ avatar, name, isOnline }) {
+export default function FriendListItem({ avatar, name, isOnline, id }) {
+  const statusClassName = isOnline ? css.online : css.offline;
   return (
-      <li className={css.item}>
-      <span className={`${css.status} ${isOnline ? 'online' : 'offline'}`}></span>
+      <li key={id} className={css.item}>
+      <span className={`${css.status} ${statusClassName}`}></span>
           <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
           <p className={css.name}>{name}</p>
     </li>
